e4f9f8d9484a4d418c1a6b06924c2efb221183c4
banelatika <100022963+banelatika@users.noreply.github.com>
2022-06-15 16:32:03 +0530

    first commit

---
+++ qr_code_generator.py
@@ -0,0 +1,10 @@
+from tkinter import TRUE
+from turtle import bgcolor, fillcolor
+import qrcode as qr
+from PIL import Image
+qr = qr.QRCode(version=1,
+error_correction=qr.ERROR_CORRECT_H,box_size=18, border=4) 
+qr.add_data("https://youtu.be/Jey1GH8CERI")
+qr.make(fit=TRUE)
+img=qr.make_image(fillcolor="red",bgcolor ="blue" )
+img.save("wscube_web.png")
